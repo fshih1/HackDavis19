23761e4415b0bebc1b6a7ea8c5a6bd0a5d947bd4
Ez <fshih@ucdavis.edu>
2019-02-09 23:29:28 -0800

    current progress

--- interface.js
+++ interface.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
-    
+
     $('.btn').on('click', function() {
     	// $('button').removeClass('active');
     	$(this).addClass('active');
@@ -7,7 +7,6 @@ $( document ).ready(function() {
 	})
 
 });
-
 
 $('#debugSelector').click(function() {
     	// $('button').removeClass('active');
@@ -22,12 +21,7 @@ $('#show-results').click(function() {
 })
 
 function getInput(){
-
 	var list = [];
-
-	// var e = $("info1");	
-	// var strUser = e.options[e.selectedIndex].value;
-	// var strUser1 = $("#info1").val()
 	list.push($("#info1").val())
 	list.push($("#info2").val())
 	list.push($("#info3").val())
@@ -37,27 +31,6 @@ function getInput(){
 	list.push($("#info7").val())
 	list.push($("#info8").val())
 	list.push($("#info9").val())
-
-	// var e = document.getElementById("info2");
-	// var strUser = e.options[e.selectedIndex].value;
-	// list.push("strUser")
-
-	// var e = document.getElementById("info3");
-	// var strUser = e.options[e.selectedIndex].value;
-	// list.push("strUser")
-
-	// var e = document.getElementById("info4");
-	// var strUser = e.options[e.selectedIndex].value;
-	// list.push("strUser")
-
-	// var e = document.getElementById("info5");
-	// var strUser = e.options[e.selectedIndex].value;
-	// list.push("strUser")
-
-	// var e = document.getElementById("info6");
-	// var strUser = e.options[e.selectedIndex].value;
-	// list.push("strUser")
-
 	console.log("list is " + list);
 }
 
